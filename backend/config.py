7f97a58b2280711d892d1500686124a9ec38271f
Nelly <mwendwanelly042@gmail.com>
2025-12-15 21:31:34 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -5,3 +5,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///digital_guides.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
+
+    
