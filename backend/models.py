4d035b4f6e89d1a1958b7bacca224a43e916ba87
Nelly <mwendwanelly042@gmail.com>
2025-12-17 20:59:49 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -164,3 +164,5 @@ class Booking(db.Model):
             'experience': self.experience.to_dict() if self.experience else None,
             'experience_date': self.experience_date.to_dict() if self.experience_date else None
         }
+
+        
